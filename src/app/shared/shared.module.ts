25c15637a21a1ac2aab2617f68f4f01f09f1922e
ELansdowne <elansdowneuk@gmail.com>
2019-02-20 01:31:42 +0530

    added layout and header components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,111 @@
+import { CdkTableModule } from '@angular/cdk/table';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  DateAdapter,
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  NativeDateAdapter
+} from '@angular/material';
+import { LayoutNavComponent } from './components/layout-nav/layout-nav.component';
+import { HeaderComponent } from './components/header/header.component';
+const ANGULAR_MODULE: any[] = [FormsModule, ReactiveFormsModule];
+const MATERIAL_MODULES: any[] = [
+  CdkTableModule,
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatProgressBarModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSidenavModule,
+  MatStepperModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDividerModule,
+  MatAutocompleteModule,
+  MatStepperModule,
+  MatBadgeModule
+];
+const LAYOUT_COMPONENTS: any[] = [
+  // LayoutComponent,
+  LayoutNavComponent
+  // NavigationDrawerComponent,
+  // LoaderComponent,
+  // FooterComponent
+];
+const SHARED_COMPONENTS: any[] = [
+  // SafeHtmlPipe,
+  // PricePipe,
+  // GroupByPipe,
+  // SubscribersChainComponent,
+  // NoDataAvailableComponent,
+  // AddressSuggestionComponent
+];
+
+@NgModule({
+  imports: [FormsModule, CommonModule, ANGULAR_MODULE, MATERIAL_MODULES],
+  declarations: [
+    LAYOUT_COMPONENTS,
+    SHARED_COMPONENTS,
+    LayoutNavComponent,
+    HeaderComponent
+  ],
+  entryComponents: [],
+  providers: [
+    { provide: DateAdapter, useClass: NativeDateAdapter }
+    // LoaderService,
+    // FileHandlerService
+  ],
+  exports: [
+    ANGULAR_MODULE,
+    MATERIAL_MODULES,
+    LAYOUT_COMPONENTS,
+    SHARED_COMPONENTS
+  ]
+})
+export class SharedModule {}

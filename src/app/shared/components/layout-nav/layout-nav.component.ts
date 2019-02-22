060d1c3de934a3d6f71f4c1888cec344d4eb56f9
Ankit <elansdowneuk@gmail.com>
2019-02-22 12:50:14 +0530

    NO TASK styles added

--- src/app/shared/components/layout-nav/layout-nav.component.ts
+++ src/app/shared/components/layout-nav/layout-nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout-nav',
   templateUrl: './layout-nav.component.html',
-  styleUrls: ['./layout-nav.component.scss']
+  styleUrls: ['./layout-nav.component.css']
 })
 export class LayoutNavComponent implements OnInit {
   constructor() {}

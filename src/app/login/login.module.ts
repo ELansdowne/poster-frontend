a95c3b0326e76b2f78d5b60556ec8867e05f6a35
Ankit <elansdowneuk@gmail.com>
2019-02-22 19:31:05 +0530

    NO TASK added validations

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -5,9 +5,10 @@ import { LoginComponent } from './login/login.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
-  imports: [SharedModule, LoginRoutingModule],
+  imports: [CommonModule,SharedModule, LoginRoutingModule],
   declarations: [LoginComponent, SignInComponent, SignUpComponent, ForgotPasswordComponent],
   exports: [],
   providers: []
--- src/app/login/sign-in/sign-in.component.ts
+++ src/app/login/sign-in/sign-in.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.component.html",
+  styleUrls: ["./sign-in.component.css"]
 })
 export class SignInComponent implements OnInit {
-  constructor() {}
+  constructor(public formBuilder: FormBuilder) {}
 
+  public signInForm: FormGroup = this.formBuilder.group({
+    email: ["", Validators.email],
+    password: ["", Validators.required]
+  });
   ngOnInit(): void {}
+
+  onSignIn(): void {
+    console.log("here", this.signInForm);
+  }
 }
